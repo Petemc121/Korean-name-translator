f1879db6192b11ce71578443aebf09a82dec6f06
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-21 18:26:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,268 @@
+const hanguelConst = [
+  "ㄴ",
+  "ㄷ",
+  "ㅁ",
+  "ㅂ",
+  "ㅅ",
+  "ㅈ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+  "ㄱ",
+  "ㄹ",
+  "ㅇ",
+];
 
-const hanguelConst = ["ㄱ", "ㄴ", "ㄷ", "ㅁ", "ㅂ", "ㅅ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "ㄹ", "ㅇ"];
-const englishConst = ["g", "n", "d", "m", "b", "s", "j", "ch", "k", "t", "p", "h", "l", "r", "ng", ""];
-const hanguelVowels = ["ㅏ", "ㅑ", "ㅓ", "ㅕ", "ㅗ", "ㅛ", "ㅜ", "ㅠ", "ㅡ", "ㅣ"];
-const englishVowels = ["a", "ya", "eo", "yeo", "o", "yo", "u", "yu", "eu", "i"];
-const hanguelVowels2 = ["ㅐ", "ㅒ", "ㅔ", "ㅖ", "ㅘ", "ㅙ", "ㅚ", "ㅝ", "ㅞ", "ㅟ", "ㅢ"];
-const englishVowels2 = ["ae", "ae", "ae", "yae", "wa", "wae", "wi", "weo", "wue", "wui", "eui"];
-const button = document.getElementById('button');
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const hanguelDoubleConst = ["ㄸ", "ㅃ", "ㅆ", "ㅉ", "ㄲ"];
+
+const englishDoubleConst = ["dd", "bb", "ss", "jj", "gg"];
+
+const englishConst = [
+  "n",
+  "d",
+  "m",
+  "b",
+  "s",
+  "j",
+  "ch",
+  "k",
+  "t",
+  "p",
+  "h",
+  "g",
+  "l",
+  "r",
+  "ng",
+  "",
+];
+const hanguelVowels = [
+  "ㅏ",
+  "ㅑ",
+  "ㅓ",
+  "ㅕ",
+  "ㅗ",
+  "ㅛ",
+  "ㅜ",
+  "ㅠ",
+  "ㅡ",
+  "ㅣ",
+  "ㅐ",
+  "ㅒ",
+  "ㅔ",
+  "ㅖ",
+];
+const englishVowels = [
+  "a",
+  "ya",
+  "eo",
+  "yeo",
+  "o",
+  "yo",
+  "u",
+  "yu",
+  "eu",
+  "i",
+  "ae",
+  "yae",
+  "ae",
+  "yae",
+];
+const hanguelVowels2 = ["ㅘ", "ㅙ", "ㅚ", "ㅝ", "ㅞ", "ㅟ", "ㅢ"];
+const englishVowels2 = ["wa", "wae", "wi", "weo", "wue", "wui", "eui"];
+const button = document.getElementById("button");
+const output = document.getElementById("output");
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 const format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/; // special character regex
-
 
 //click event for button
 
-button.addEventListener('click', function () {
-    const nameIn = document.getElementById('nameIn').value
-    const nameOut = document.getElementById('output').value
+button.addEventListener("click", function () {
+  const nameIn = document.getElementById("nameIn").value;
+  const nameOut = document.getElementById("output").value;
 
-    error = false;
+  error = false;
 
-    //name input validation
+  //name input validation
 
-    if (nameIn != "") {
+  if (nameIn != "") {
+    toString(nameIn);
 
-        toString(nameIn)
+    for (let i = 0; i < nameIn.length; i++) {
+      //test if name is korean letters only
 
-        for (let i = 0; i < nameIn.length; i++) {
-
-            //test if name is korean letters only
-
-            if (alphabet.includes(nameIn[i]) || isNaN(nameIn[i]) == false || format.test(nameIn[i])) {
-                error = true
-            }
-        }
-        if (error == true) {
-            alert("please enter korean letters only")
-        } else {
-
-            // this is A function built into hangeul.js, a library that disects Korean letters into their separated symbols
-
-            disasseble = Hangul.disassemble(nameIn);
-            console.log(disasseble);
-
-
-
-        }
-    } else {
-        alert("Please enter your korean name")
+      if (
+        alphabet.includes(nameIn[i]) ||
+        isNaN(nameIn[i]) == false ||
+        format.test(nameIn[i])
+      ) {
+        error = true;
+      }
     }
+    if (error == true) {
+      alert("please enter korean letters only");
+    } else {
+      // this is A function built into hangeul.js, a library that disects Korean letters into their separated symbols
 
-})
+      disassemble = Hangul.d(nameIn);
+      console.log(disassemble);
+      conReturn = korToEng(disassemble);
+    }
+  } else {
+    alert("Please enter your korean name");
+  }
+});
 
-// function to convert each Korean letter into an english letter 
+// function to convert each Korean letter into an english letter
 
 function korToEng(input) {
+  engName = [];
+  for (var i; i < input.length; i++) {
+    for (var j = 0; j < input.length; j++) {
+      if (hanguelConst.includes(input[i][j])) {
+        letterConst = mapConst(input[i][j]);
 
-    for (var i = 0; i < input.length; i++) {
-
-        if (hanguelConst.includes(input[i])) {
-
-            letterTest = mapConst(input[i]);
-
-            if (letterTest == false) {
-                if (input[i] == "ㅇ") {
-                    if (hangeulVowels.includes(input[i + 1])) {
-                        let engLetter = "";
-                        return engLetter
-                    } else {
-                        return "ng";
-                    }
-                } else if (input[i] == "ㄹ") {
-                    if (input[i] == input[0]) {
-                        return "R"
-                    } else if (hangeulVowels.includes(input[i - 1])) {
-                        return "r"
-                    } else {
-                        return "l"
-                    }
-                }
+        if (letterConst === false) {
+          if (input[i][j] === "ㅇ") {
+            if (hanguelVowels.includes(input[i + 1])) {
+              engName = engName;
+            } else {
+              engName.push("ng");
             }
-
+          } else if (input[i][j] === "ㄹ") {
+            if (i == 0) {
+              engName.push("R");
+            } else if (hanguelVowels.includes(input[i - 1])) {
+              engName.push("r");
+            } else {
+              engName.push("l");
+            }
+          } else if (input[i][j] == "ㄱ") {
+            if (i == 0) {
+              engName.push("K");
+            } else {
+              engName.push("g");
+            }
+          }
+        } else {
+          engName.push(letterConst);
         }
+      } else if (hanguelDoubleConst.includes(input[i][j])) {
+        if (hanguelConst.includes(input[i + 1])) {
+          if (input[i + 1] == "ㅇ") {
+            doubleConst = mapDoubleConst(input[i][j]);
+            engName.push(doubleConst);
+          } else {
+            if (input[i][j] == "ㅆ") {
+              engName.push("t");
+            } else if (input[i][j] == "ㄸ") {
+              engName.push("t");
+            } else if (input[i][j] == "ㅃ") {
+              engName.push("pp");
+            } else if (input[i][j] == "ㅉ") {
+              engName.push("t");
+            } else if (input[i][j] == "ㄲ") {
+              engName.push("k");
+            }
+          }
+        } else {
+          doubleConst = mapDoubleConst(input[i][j]);
+          engName.push(doubleConst);
+        }
+      } else if (hanguelVowels.includes(input[i][j])) {
+        if (input[i][j] === "ㅗ") {
+          if (input[i + 1] === "ㅏ") {
+            engName.push("w");
+          } else if (input[i + 1] === "ㅐ") {
+            engName.push("w");
+          } else if (input[i + 1] === "ㅣ") {
+            engName.push("o");
+          } else {
+            letterVowel = mapVowel(input[i][j]);
+            engName.push(letterVowel);
+          }
+        } else if (input[i][j] === "ㅜ") {
+          if (input[i + 1] === "ㅓ") {
+            engName.push("w");
+          } else if (input[i + 1] === "ㅔ") {
+            engName.push("w");
+          } else if (input[i - 1] === "ㅇ") {
+            engName.push("woo");
+          } else {
+            letterVowel = mapVowel(input[i][j]);
+            engName.push(letterVowel);
+          }
+        } else if (input[i][j] === "ㅡ") {
+          if (input[i + 1] === "ㅣ") {
+            engName.push("eu");
+          } else {
+            letterVowel = mapVowel(input[i][j]);
+            engName.push(letterVowel);
+          }
+        } else {
+          letterVowel = mapVowel(input[i][j]);
+          engName.push(letterVowel);
+        }
+      }
     }
+
+    engName.push(" ");
+  }
+  finalName = engName.join("");
+  output.innerHTML = finalName;
 }
 
 //function to convert letters exluding "ㅇ" and "ㄹ" (these symbols convert to more than one english symbol depending on their position)
 
 function mapConst(letter) {
+  if (letter != "ㄱ" && letter != "ㅇ" && letter != "ㄹ") {
+    position = hanguelConst.indexOf(letter);
 
-    if (letter != "ㅇ" || letter != "ㄹ") {
-        position = hanguelConst.indexOf(letter)
+    newletter = englishConst[position];
+    return newletter;
+  } else {
+    return false;
+  }
+}
 
-        newletter = englishConst[position];
-        return newletter
+function mapDoubleConst(letter) {
+  position = hanguelDoubleConst.indexOf(letter);
 
-    } else {
-        return false
-    }
+  newletter = englishDoubleConst[position];
+  return newletter;
+}
+
+function mapVowel(letter) {
+  position = hanguelVowels.indexOf(letter);
+
+  newletter = englishVowels[position];
+  return newletter;
 }
